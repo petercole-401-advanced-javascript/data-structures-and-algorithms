f8bf7cd53fe574232d8029050d18dd93990692f3
‘peter-robert-cole’ <petercole3@gmail.com>
2020-02-15 14:09:10 -0800

    work on link-list

--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -11,24 +11,49 @@ class LinkedList {  // create list
   constructor(head) {
     this.head = head;
   }
+
   insert(value) {
     const node = new Node(value, null)
     node.next = this.head;
     this.head = node;
   }
-  // includes(); // placeholder
+
+  printList() {
+    let current = this.head;
+    while (current.next !== null) {
+      console.log(`Value: ${current.value}`);
+      current = current.next;
+    }
+  };
+
+  includes(input) {
+    let current = this.head;
+    while (current.next !== null) {
+      console.log({current});
+      if (current.value === input) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  };
+
   // toString(); // placeholder
   append(node) { // and add a node at the end
     let current = this.head;
-    while (current !== null) {
+    while (current.next !== null) {
       current = current.next;
     }
     current.next = node;
   }
 }
 
-// const elListo = new LinkedList(1);
+const theList = new LinkedList(new Node(-1, null));
 
-// elListo.append(5);
+for(let i = 0; i < 5; i++){
+  theList.insert(i);
+}
+
+console.log(theList.includes(4));
 
 module.exports = LinkedList;
