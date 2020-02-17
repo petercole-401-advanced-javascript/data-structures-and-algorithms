98fde94a98e46d9d99e3c21789e2dbc7517ee281
‘peter-robert-cole’ <petercole3@gmail.com>
2020-02-16 23:16:41 -0800

    7 tests passing, done

--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -20,11 +20,17 @@ class LinkedList {  // create list
 
   includes(input) { // is a value in the list? let's traverse and check
     let current = this.head;
-    while (current.next !== null) {
+    if (current === undefined) {
+      return false;
+    }
+    do {
       if (current.value === input) {
         return true;
       }
       current = current.next;
+    } while (current.next != null);
+    if (current.value === input) {
+      return true;
     }
     return false;
   };
@@ -32,40 +38,36 @@ class LinkedList {  // create list
   toString() { // { c } -> { b } -> { a } -> NULL
     let current = this.head;
     let str = '';
+    if (current === undefined) {
+      return 'Easy there fella, you\'ve got an empty list!';
+    }
     do {
       str = str.concat(`{ ${current.value} } -> `);
       current = current.next;
-    } while (current.next !== null);
+    } while (current.next != null);
       str = str.concat(`{ ${current.value} } -> NULL`);
     return str;
   };
 
-  printFull() {
-    let current = this.head;
-    let str = '';
-    do {
-      str = str.concat(`{ ${current.value} }  -> `);
-      current = current.next;
-    } while (current.next !== null);
-      str = str.concat(`{ ${current.value} } -> NULL`);
-    return str;
-  }
+  printHead() { // extra method, head check
+    return this.head;
+  };
 
-  appendAtEnd(node) { // extra method, not part of challenge
+  appendAtEnd(node) { // extra method, add node to the end, might have an OBOE
     let current = this.head;
-    while (current.next !== null){
+    while (current.next != null){
       current = current.next;
     }
     current.next = node;
-  }
+  };
 }
 
 // ------ Helpful
 // console.log({current});
 
 // ------ Build a Tester List
-const theList = new LinkedList(new Node(0, null));
-for (let i = 1; i <= 1; i++){theList.insert(i)};
+// const theList = new LinkedList();
+// for (let i = 0; i <= 2; i++){theList.insert(i)};
 
 // ------ Test includes()
 // console.log(theList.includes(4));
@@ -73,9 +75,7 @@ for (let i = 1; i <= 1; i++){theList.insert(i)};
 // ------ Test toString()
 // console.log(theList.toString());
 
-// ------ Test printFull()
-console.log(theList.printFull());
-let hed = theList.head;
-console.log(hed);
+// ------ Test printHead()
+// console.log('Head: ', theList.printHead());
 
 module.exports = {LinkedList, Node};
