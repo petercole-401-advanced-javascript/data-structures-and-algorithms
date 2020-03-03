2dc6a321fd498fa91cae8308f6acce41f9551b9d
‘peter-robert-cole’ <petercole3@gmail.com>
2020-03-02 23:23:47 -0800

    working on new tree function

--- challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -15,22 +15,47 @@ class BinaryTree {
   preOrder(root, stack = []) {
     let current = root;
     if (current === null) return;
-    stack.push(this.modulusToSring(current.value));
+    stack.push(current.value);
     if (current.left !== null) this.preOrder(current.left, stack);
     if (current.right !== null) this.preOrder(current.right, stack);
     return stack;
   }
   modulusToSring(value) {
-    if (value % 3 === 0 && value % 5 === 0){
+    if (value % 3 === 0 && value % 5 === 0) {
       return 'FizzBuzz';
     } else if (value % 3 === 0) {
       return 'Fizz';
     } else if (value % 5 === 0) {
-        return 'Buzz'
+      return 'Buzz'
     } else {
-        return value.toString();
+      return value.toString();
     }
   }
 }
+
+// THIS NEEDS WORK vvv
+
+let fizzTree = new BinaryTree(new Node())
+
+function FizzBuzzTree(root, stack = []) {
+  let current = root;
+  if (current === null) return;
+  stack.push(current.value);
+  fizzTree.current = new Node(current.value);
+  if (current.left !== null) {
+    fizzTree.left = new Node(fizzTree.modulusToSring(current.value));
+    FizzBuzzTree(current.left, stack);
+  }
+  if (current.right !== null) {
+    fizzTree.right = new Node(fizzTree.modulusToSring(current.value));
+    FizzBuzzTree(current.right, stack);
+  }
+  return fizzTree;
+}
+
+const testTree = new Node(10, new Node(6, new Node(3, null, null), new Node(8, null, null)), new Node(15, null, new Node(20, null, null)));
+
+console.log(FizzBuzzTree(testTree));
+
 
 module.exports = { BinaryTree, Node };
